7cff1c5b437723b01476b187bfbc95335b8270ce
Yenesey2001 <yeneseyclase@gmail.com>
2025-02-10 10:22:08 +0000

    Contador hechogit add .

---
+++ src/js/components/Counter.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Counter({ seconds }) {
+  const contador = seconds.toString().padStart(6, "0");
+
+
+  return (
+    <div>
+      <h1>{contador}</h1>
+    </div>
+  );
+}
+
+export default Counter;
